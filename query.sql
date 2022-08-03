c3010ce77694fc87118b0bed4996f0c332e47530
Muhammad Deril <49135753+derilkillms@users.noreply.github.com>
2022-08-03 12:02:54 +0700

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,4 +1,4 @@
-//GET COURSE AND CATEGORY WPLMS
+/** GET COURSE AND CATEGORY WPLMS */
 SELECT wps.ID,wps.post_title AS course_name,wps.post_name,wtm.term_id AS cat_id,wtm.name AS category FROM wp_posts wps 
 JOIN wp_term_relationships wtr ON wps.ID=wtr.object_id 
 JOIN wp_terms wtm ON wtm.term_id=wtr.term_taxonomy_id 
